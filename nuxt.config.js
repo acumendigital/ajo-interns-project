c8c7880f5fbbbd72eea320b48902c00e6b8a16f3
SualaDev <81395134+SualaDev@users.noreply.github.com>
2022-04-26 18:02:33 +0100

    made changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,21 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ajo-interns-project",
+    title: 'ajo-interns-project',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,42 +32,42 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
+    '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    "@nuxtjs/pwa",
-    //Nuxt Fontawesome
+    '@nuxtjs/pwa',
+    // Nuxt Fontawesome
     [
-      "nuxt-fontawesome",
+      'nuxt-fontawesome',
       {
         imports: [
           {
-            set: "@fortawesome/free-solid-svg-icons",
-            icons: ["fas"],
+            set: '@fortawesome/free-solid-svg-icons',
+            icons: ['fas']
           },
           {
-            set: "@fortawesome/free-regular-svg-icons",
-            icons: ["far"],
+            set: '@fortawesome/free-regular-svg-icons',
+            icons: ['far']
           },
           {
-            set: "@fortawesome/free-brands-svg-icons",
-            icons: ["fab"],
-          },
-        ],
-      },
-    ],
+            set: '@fortawesome/free-brands-svg-icons',
+            icons: ['fab']
+          }
+        ]
+      }
+    ]
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "/",
+    baseURL: '/'
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: "en",
-    },
+      lang: 'en'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -75,5 +75,5 @@ export default {
 
   // Transitions
 
-  pageTransition: "page",
-};
+  pageTransition: 'page'
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,118 +1,44 @@
 <template>
-  <main>
-    <div class="logo">
-      <img src="../assets/img/ajo-logo.png" alt="" />
+  <div class="container">
+    <NavBar />
+    <div class="background-images">
+      <div class="phones" />
+      <div class="store" />
     </div>
-    <div class="title">
-      <!-- <img src="../assets/img/ajo-text.png" alt="" /> -->
-      <h1>AJO</h1>
-    </div>
-    <div class="text">
-      <h1>Welcome back, Folashade</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </div>
-    <div class="button">
-      <a href="/home"> <TheButton title=" Get Started" value="whiteBgLg" /></a>
-    </div>
-  </main>
+    <Details />
+    <Cards />
+    <Review />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import TheButton from "~/components/TheButton.vue";
+import Footer from '~/components/Footer.vue'
 export default {
-  name: "IndexPage",
-  transition: "start",
-  components: { TheButton },
-};
+  name: 'IndexPage',
+  components: { Footer }
+}
 </script>
-<style lang="scss" scoped>
-@media screen and (max-width: 428px) {
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 428px;
-    height: 926px;
-    background-image: linear-gradient(to bottom, #041a7acc, #041a7acc 80%),
-      url("../assets/img/home-bg.png");
-    background-position: center;
-    background-size: 542px 926px;
-    background-repeat: no-repeat;
-    color: #fff;
-    .logo {
-      margin-top: 105px;
-      img {
-        position: relative;
-        top: 55px;
-        width: 180px;
-        height: 185px;
-      }
-    }
-    .title {
-      font-family: "AirbnbCereal_W_Bd";
-      letter-spacing: 2px;
-    }
-    .text {
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      margin-top: 45px;
-      text-align: center;
-      margin-bottom: 94px;
-      h1 {
-        position: relative;
-        font-style: normal;
-        font-weight: 700;
-        font-size: 24px;
-        font-family: "AirbnbCereal_W_md";
-        width: 305px;
-      }
-      h1::after {
-        content: "";
-        position: absolute;
-        height: 4px;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background: #fcf300;
-        transform: scaleX(0);
-        transform-origin: left;
-        transition: transform 250ms ease;
-      }
-
-      p {
-        font-family: "AirbnbCereal_W_lt";
-        font-size: 14px;
-        font-weight: 300;
-        width: 343px;
-      }
-    }
-
-    .text:hover {
-      h1::after {
-        transform: scaleX(1);
-      }
-    }
-    .button {
-      margin-bottom: 92px;
-      a {
-        text-decoration: none;
-        font-weight: 500;
-        font-family: "AirbnbCereal_W_lt";
-      }
-    }
-  }
+<style scoped>
+.container{
+  max-width: 1440px;
+  margin: 0 auto;
+  position: relative;
 }
-
-.start-enter-active,
-.start-leave-active {
-  transition: opacity 0.5s;
+.phones{
+  background-image: url(~assets/images/phone.png);
+  position: absolute;
+  width: 580.49px;
+  height: 701.75px;
+  top: 443px;
+  left: 430px;
 }
-.start-enter,
-.start-leave-active {
-  opacity: 0;
+.store{
+  background-image: url(~assets/images/stores.png);
+  position: absolute;
+  width: 535px;
+  height: 84px;
+  top: 1241px;
+  left: 464px;
 }
 </style>
