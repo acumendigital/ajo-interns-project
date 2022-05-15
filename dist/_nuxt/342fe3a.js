208b5fde458fd376bfc47002660a03d63c7539b0
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-05-15 18:10:24 -0400

    :card_file_box: deploy 6.2

--- dist/_nuxt/342fe3a.js
+++ dist/_nuxt/342fe3a.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:dist/_nuxt/342fe3a.js
 (window.webpackJsonp=window.webpackJsonp||[]).push([[32],{460:function(n,e,t){"use strict";t.r(e);var l={},c=t(36),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
-========
-(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{458:function(n,e,t){"use strict";t.r(e);var l={},c=t(36),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
->>>>>>>> 118d73d57e2bddfa2c687741d0ce5ff798d9e9a8:dist/_nuxt/3352d3d.js
