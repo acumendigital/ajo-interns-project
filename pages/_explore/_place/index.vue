77ed25d2a860fa844f533c48f3b3e2fb0170b8c1
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2022-05-15 23:25:51 +0100

    Pulling from upstream

--- pages/_explore/_place/index.vue
+++ pages/_explore/_place/index.vue
@@ -194,9 +194,6 @@ export default {
       }
       return 'See All'
     },
-    allPlaceImgs() {
-      return this.placeDetailData.photos
-    },
     similarPlaces() {
       return this.similarPlacesData
     },
